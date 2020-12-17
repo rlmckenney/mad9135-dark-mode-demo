3c90e89d73be758fdf0c5d4a8a0890a87aa77f8c
Robert McKenney <robert@mckenney.ca>
2020-12-17 09:59:09 -0500

    Update to fetch from swapi.dev

--- context/moviesContext.js
+++ context/moviesContext.js
@@ -1,25 +1,43 @@
 import React from 'react'
 
-const MoviesContext = React.createContext()
-
-function MoviesProvider(props) {
-  const [movies, setMovies] = React.useState([])
-
-  React.useEffect(() => {
-    ;(async function () {
-      const {movieData} = await import('../data/swapi.films')
-      setMovies(movieData)
-    })()
-  }, [])
-
-  return <MoviesContext.Provider value={[movies]} {...props} />
+export const FETCH_STATUS = {
+  idle: 0,
+  pending: 1,
+  resolved: 2,
+  rejected: 3
 }
 
-function useMovies() {
+const MoviesContext = React.createContext()
+
+export function MoviesProvider(props) {
+  const [movies, setMovies] = React.useState([])
+  const [status, setStatus] = React.useState(FETCH_STATUS.idle)
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() => {
+    fetchMovies()
+  }, [fetchMovies])
+
+  async function fetchMovies() {
+    setStatus(FETCH_STATUS.pending)
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      if (!response.ok) throw new Error('Could not fetch data', response)
+      const responseBody = await response.json()
+      setMovies(responseBody.results)
+      setStatus(FETCH_STATUS.resolved)
+    } catch (err) {
+      setError(err)
+      setStatus(FETCH_STATUS.rejected)
+    }
+  }
+
+  return <MoviesContext.Provider value={[movies, status, error]} {...props} />
+}
+
+export function useMovies() {
   const context = React.useContext(MoviesContext)
   if (!context)
     throw new Error('useMovies hook must be called within a MoviesProvider')
   return context
 }
-
-export {MoviesProvider, useMovies}
